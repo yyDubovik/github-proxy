1dba33b6b9ba2eb27f1f040928e2faacb97b3017
Yauheni Dubovik <56396765+yyDubovik@users.noreply.github.com>
2026-01-07 14:56:53 +0100

    Refactor: Enable parallel execution
    Added StopWatch for performance measurement. Configured WireMock 1s delay.

--- src/test/java/org/example/githubproxy/GithubProxyApplicationTests.java
+++ src/test/java/org/example/githubproxy/GithubProxyApplicationTests.java
@@ -1,6 +1,7 @@
 package org.example.githubproxy;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,30 +25,54 @@ class GithubProxyApplicationTests {
 
     @Test
     void shouldReturnUserRepositoriesExcludingForks() throws Exception {
+        int delayMs = 1000;
+
         stubFor(WireMock.get(urlPathEqualTo("/users/test-user/repos"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
+                        .withFixedDelay(delayMs)
                         .withBody("""
                                 [
                                     {"name": "repo-1", "owner": {"login": "test-user"}, "fork": false},
-                                    {"name": "repo-fork", "owner": {"login": "test-user"}, "fork": true}
+                                    {"name": "repo-fork", "owner": {"login": "test-user"}, "fork": true},
+                                    {"name": "repo-2", "owner": {"login": "test-user"}, "fork": false}
                                 ]
                                 """)));
 
         stubFor(WireMock.get(urlPathEqualTo("/repos/test-user/repo-1/branches"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "application/json")
+                        .withFixedDelay(delayMs)
                         .withBody("""
                                 [{"name": "main", "commit": {"sha": "sha123"}}]
                                 """)));
 
+        stubFor(WireMock.get(urlPathEqualTo("/repos/test-user/repo-2/branches"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withFixedDelay(delayMs)
+                        .withBody("""
+                                [{"name": "main", "commit": {"sha": "sha456"}}]
+                                """)));
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         mockMvc.perform(get("/api/repos/test-user")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$.length()").value(2))
                 .andExpect(jsonPath("$[0].repositoryName").value("repo-1"))
+                .andExpect(jsonPath("$[1].repositoryName").value("repo-2"))
                 .andExpect(jsonPath("$[0].branches[0].name").value("main"))
                 .andExpect(jsonPath("$[0].branches[0].lastCommitSha").value("sha123"));
+
+        stopWatch.stop();
+        long time = stopWatch.getTime();
+        System.out.println("Time: " + time);
+
+        verify(3, getRequestedFor(urlMatching(".*")));
+        assertThat(time).isBetween(2000L, 3100L);
     }
 
     @Test
